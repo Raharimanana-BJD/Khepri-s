cc709d0ad693d288aa6a468dae5199b402aa6f1b
RAHARIMANANA-BJD <brayjuanico@gmail.com>
2024-07-23 00:30:54 +0300

    add_splashScreen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
+// layout.tsx
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { Header } from "../components";
-import { cn } from "../lib/utils";
 import Footer from "@/components/Footer";
+import SplashScreen from "@/components/SplashScreen";
 import { Toaster } from "react-hot-toast";
+import { cn } from "../lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,11 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={cn("min-h-[100dvh] bg-grey-100 dark:bg-secondary-500 antialiased font-sans] duration-100 transition-all",inter.className)}>
+      <body className={cn("min-h-[100dvh] bg-grey-100 dark:bg-secondary-500 antialiased font-sans] duration-100 transition-all", inter.className)}>
+        <SplashScreen />
         <Header />
         {children}
-        <Footer/>
-        <Toaster position="bottom-right"/>
+        <Footer />
+        <Toaster position="bottom-right" />
       </body>
     </html>
   );
